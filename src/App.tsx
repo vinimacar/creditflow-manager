46ddbef6e86b4208e25e8a89957cc2da1b2d6eea
vinimacar <vinicius.machado.carvalho@educacao.mg.gov.br>
2026-01-08 01:10:10 -0300

    Fix: adicionar basename ao BrowserRouter para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/creditflow-manager">
         <Routes>
           <Route
             path="/"
