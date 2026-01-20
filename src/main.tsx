f3b03652f849e437516fac600ad66c255f2e3ceb
vinimacar <vinicius.machado.carvalho@educacao.mg.gov.br>
2026-01-20 18:41:25 -0300

    chore: Forçar rebuild para atualizar cache do GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Deploy: 2026-01-20 18:40 - Build v2 - Fix DataTable customActions undefined
 createRoot(document.getElementById("root")!).render(<App />);
